d4cccc47d2898729457037e044d3e2187bf37882
Savanna Williams <wp192022@gmail.com>
2026-02-20 09:44:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+async function getServices() {
+    try{
+        //get data from server
+        const response = await fetch("db.json")
+        const data = await response.json()
+        const services = data.catbox_services
+        console.log(services)
+
+        //add services to DOM
+        const serviceList = document.querySelector(".serviceList")
+        for (let serviceData of services) {
+            let service = document.createElement("div")
+            serviceList.appendChild(service)
+            service.classList.add("serviceBox")
+            service.innerHTML = `<h2>${serviceData.product_name}</h2><p>$${serviceData.price}</p>`
+        }
+    }catch (err){
+        console.log(err)
+    }
+}
+getServices()
+
+//Hamburger menu
+const hamburgijr = document.querySelector(".burgerMenu")
+const hamburgi = document.querySelector(".burgerButton")
+hamburgijr.classList.add("burgerGoAway")
+hamburgi.addEventListener("click",()=>{
+    hamburgijr.classList.toggle("burgerGoAway")
+})
